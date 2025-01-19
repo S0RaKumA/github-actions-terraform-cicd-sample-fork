c354be287f74480b291c4a7b62d8693a6790d181
S0RaKumA <71373204+S0RaKumA@users.noreply.github.com>
2025-01-19 19:34:25 +0800

    commented out the backend and test actions

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 terraform {
-  backend "s3" {
-    bucket         = "s306022024"
-    key            = "terraform.tfstate"
-    region         = "us-east-1"
-  }
+  #backend "s3" {
+  #  bucket         = "s306022024"
+  #  key            = "terraform.tfstate"
+  #  region         = "us-east-1"
+  #}
 }
 
 provider "aws" {
